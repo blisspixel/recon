6c73f04b22fe238670a0309fb53363bf6bcffc4a
Nick Seal <32712898+blisspixel@users.noreply.github.com>
2026-08-19 09:31:29 -0700

    v2.17.1: release

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -13,7 +13,7 @@
 $ErrorActionPreference = "Stop"
 
 $Package = "recon-tool"
-$Version = "2.17.0"
+$Version = "2.17.1"
 $Spec = "$Package==$Version"
 $Cli = "recon"
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -14,7 +14,7 @@
 set -euo pipefail
 
 PACKAGE="recon-tool"
-VERSION="2.17.0"
+VERSION="2.17.1"
 SPEC="${PACKAGE}==${VERSION}"
 CLI="recon"
 
